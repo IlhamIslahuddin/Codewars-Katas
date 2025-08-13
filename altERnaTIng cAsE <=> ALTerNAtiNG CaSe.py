d3a9e0c4db5d9c8509374cda5664c1573cfd22a3
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-08-13 18:38:12 +0800

    Create altERnaTIng cAsE <=> ALTerNAtiNG CaSe.py

---
+++ altERnaTIng cAsE <=> ALTerNAtiNG CaSe.py
@@ -0,0 +1,12 @@
+def to_alternating_case(string):
+    output = ""
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    for char in string:
+        if char in lowercase:
+            output += char.upper()
+        elif char in uppercase:
+            output += char.lower()
+        else:
+            output += char
+    return output

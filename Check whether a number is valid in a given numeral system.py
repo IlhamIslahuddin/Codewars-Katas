644470fa0df4bc14b79c80be929e2d60014cfaf2
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-08-21 15:00:17 +0800

    Create Check whether a number is valid in a given numeral system.py

---
+++ Check whether a number is valid in a given numeral system.py
@@ -0,0 +1,6 @@
+def validate_base(num: str, base: int) -> bool:
+    digits = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    for char in num:
+        if char not in digits[:base]:
+            return False
+    return True

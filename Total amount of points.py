5573ea8a5f9e545fd0331b3235c5c33a65f495d4
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-09-30 18:42:38 +0100

    Create Total amount of points.py

---
+++ Total amount of points.py
@@ -0,0 +1,8 @@
+def points(games):
+    total = 0
+    for game in games:
+        if int(game[0]) > int(game[-1]):
+            total += 3
+        elif int(game[0]) == int(game[-1]):
+            total += 1
+    return total
